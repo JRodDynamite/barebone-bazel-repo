86a94d6660334e3609954fce78deaf809a1855dc
Jason Estibeiro <jason.e@splore.com>
2023-02-03 18:55:45 +0800

    Additional comments and formatiing

--- backend/api/main.py
+++ backend/api/main.py
@@ -3,7 +3,11 @@ print(sys.version)
 for path in sys.path:
     print(path)
 
+# 2 ways to import
+# 1. complete project path (no additional changes needed in code)
 from backend.utils.src.utils.logging import log
+
+# 2. Need to add imports parameter in BUILD and extend_path in library's __init__ file
 from utils.logging import log
 
 import requests
